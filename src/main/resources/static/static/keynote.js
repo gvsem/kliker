a2498210d2f011b726299c45ae378b5263df11c0
Jvst Me <git@jvst.me>
2022-10-24 18:03:43 +0300

    updated keynote upload interface

--- src/main/resources/static/static/keynote.js
+++ src/main/resources/static/static/keynote.js
@@ -1,10 +1,17 @@
 const uploadFileInput = document.querySelector(".upload-file-input");
+const keynoteSettings = document.querySelector(".keynote-settings");
 const displayLink = document.querySelector(".display-link");
 const clickerLink = document.querySelector(".clicker-link");
+const displayQrElement = document.querySelector(".display-qr");
+const clickerQrElement = document.querySelector(".clicker-qr");
+const displayQr = new QRCode(displayQrElement);
+const clickerQR = new QRCode(clickerQrElement);
 
 uploadFileInput.addEventListener("change", uploadFileInputChange);
 
 function uploadFileInputChange(e) {
+    keynoteSettings.classList.add("hidden");
+
     const filesList =  e.target.files;
     if (!filesList.length) {
         return;
@@ -23,7 +30,11 @@ function uploadFileInputChange(e) {
 function uploadRequestReadyStateChange(e) {
     const xhr = e.target;
 
-    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status < 400) {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status >= 400) {
+            alert("Cannot upload this file. Is it a properly formatted PDF?");
+            return;
+        }
         const resp = JSON.parse(xhr.responseText);
         const displayUrl = window.location.href + "display/" + resp.displayId;
         const clickerUrl = window.location.href + "clicker/" + resp.clickerId;
@@ -31,5 +42,8 @@ function uploadRequestReadyStateChange(e) {
         displayLink.innerText = displayUrl;
         clickerLink.href = clickerUrl;
         clickerLink.innerText = clickerUrl;
+        displayQr.makeCode(displayUrl);
+        clickerQR.makeCode(clickerUrl);
+        keynoteSettings.classList.remove("hidden");
     }
 }
