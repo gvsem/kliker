11339112f9b5a9bbe43277afaae482f219c16756
Jvst Me <git@jvst.me>
2022-10-24 03:11:22 +0300

    added frontend

---
+++ src/main/resources/static/static/keynote.js
@@ -0,0 +1,35 @@
+const uploadFileInput = document.querySelector(".upload-file-input");
+const displayLink = document.querySelector(".display-link");
+const clickerLink = document.querySelector(".clicker-link");
+
+uploadFileInput.addEventListener("change", uploadFileInputChange);
+
+function uploadFileInputChange(e) {
+    const filesList =  e.target.files;
+    if (!filesList.length) {
+        return;
+    }
+
+    const file = filesList[0];
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", uploadRequestReadyStateChange);
+    xhr.open("POST", "api/keynote/upload");
+    xhr.send(formData);
+}
+
+function uploadRequestReadyStateChange(e) {
+    const xhr = e.target;
+
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+        const resp = JSON.parse(xhr.responseText);
+        const displayUrl = window.location.href + "display/" + resp.displayId;
+        const clickerUrl = window.location.href + "clicker/" + resp.clickerId;
+        displayLink.href = displayUrl;
+        displayLink.innerText = displayUrl;
+        clickerLink.href = clickerUrl;
+        clickerLink.innerText = clickerUrl;
+    }
+}
