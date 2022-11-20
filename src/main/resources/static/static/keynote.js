bad3d89f991dde2e9a6e3c72aa5599340168cf81
Jvst Me <git@jvst.me>
2022-11-20 20:44:27 +0300

    Add UI design to keynote page

--- src/main/resources/static/static/keynote.js
+++ src/main/resources/static/static/keynote.js
@@ -1,11 +1,16 @@
-const uploadFileInput = document.querySelector(".upload-file-input");
+const uploadFileInput = document.getElementById("upload-file-input");
+const uploadFileTextDefault = document.querySelector(".upload-file-text-default");
+const uploadFileTextOther = document.querySelector(".upload-file-text-other");
+const chosenFileName = document.querySelector(".chosen-file-name");
 const keynoteSettings = document.querySelector(".keynote-settings");
 const displayLink = document.querySelector(".display-link");
 const clickerLink = document.querySelector(".clicker-link");
 const displayQrElement = document.querySelector(".display-qr");
 const clickerQrElement = document.querySelector(".clicker-qr");
-const displayQr = new QRCode(displayQrElement);
-const clickerQR = new QRCode(clickerQrElement);
+const qrOptions = {
+    width: 250,
+    height: 250,
+};
 
 uploadFileInput.addEventListener("change", uploadFileInputChange);
 
@@ -20,6 +25,13 @@ function uploadFileInputChange(e) {
     const file = filesList[0];
     const formData = new FormData();
     formData.append("file", file);
+
+    keynoteSettings.classList.add("hidden");
+
+    chosenFileName.innerText = file.name;
+    chosenFileName.classList.remove("hidden");
+    uploadFileTextDefault.classList.add("hidden");
+    uploadFileTextOther.classList.remove("hidden");
 
     const xhr = new XMLHttpRequest();
     xhr.addEventListener("readystatechange", uploadRequestReadyStateChange);
@@ -36,14 +48,18 @@ function uploadRequestReadyStateChange(e) {
             return;
         }
         const resp = JSON.parse(xhr.responseText);
-        const displayUrl = window.location.href + "display/" + resp.displayId;
-        const clickerUrl = window.location.href + "clicker/" + resp.clickerId;
-        displayLink.href = displayUrl;
-        displayLink.innerText = displayUrl;
-        clickerLink.href = clickerUrl;
-        clickerLink.innerText = clickerUrl;
-        displayQr.makeCode(displayUrl);
-        clickerQR.makeCode(clickerUrl);
-        keynoteSettings.classList.remove("hidden");
+        displayKeynoteInfo(resp);
     }
+}
+
+function displayKeynoteInfo(keynote) {
+    const displayUrl = window.location.href + "display/" + keynote.displayId;
+    const clickerUrl = window.location.href + "clicker/" + keynote.clickerId;
+    displayLink.href = displayUrl;
+    displayLink.innerText = displayUrl;
+    clickerLink.href = clickerUrl;
+    clickerLink.innerText = clickerUrl;
+    QRCode.toCanvas(displayQrElement, displayUrl, qrOptions);
+    QRCode.toCanvas(clickerQrElement, clickerUrl, qrOptions);
+    keynoteSettings.classList.remove("hidden");
 }
